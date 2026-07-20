5b38e2e899f9b11c4b1342ceefb2c9eb4aec8456
chaehaejoong <svneh0329@naver.com>
2026-07-21 04:24:16 +0900

    add some

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -33,18 +33,30 @@ return {
                 local bufnr = args.buf
                 local opts = { buffer = bufnr, silent = true }
 
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Go to references" }))
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code action" }))
-                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
-                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
-                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Open diagnostic float" }))
-                vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, vim.tbl_extend("force", opts, { desc = "Populate location list" }))
-                vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, vim.tbl_extend("force", opts, { desc = "Add workspace folder" }))
-                vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, vim.tbl_extend("force", opts, { desc = "Remove workspace folder" }))
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition,
+                    vim.tbl_extend("force", opts, { desc = "Go to definition" }))
+                vim.keymap.set("n", "gr", vim.lsp.buf.references,
+                    vim.tbl_extend("force", opts, { desc = "Go to references" }))
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
+                    vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
+                vim.keymap.set("n", "K", vim.lsp.buf.hover,
+                    vim.tbl_extend("force", opts, { desc = "Hover documentation" }))
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,
+                    vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
+                    vim.tbl_extend("force", opts, { desc = "Code action" }))
+                vim.keymap.set("n", "[e", vim.diagnostic.goto_prev,
+                    vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
+                vim.keymap.set("n", "]e", vim.diagnostic.goto_next,
+                    vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
+                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float,
+                    vim.tbl_extend("force", opts, { desc = "Open diagnostic float" }))
+                vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist,
+                    vim.tbl_extend("force", opts, { desc = "Populate location list" }))
+                vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder,
+                    vim.tbl_extend("force", opts, { desc = "Add workspace folder" }))
+                vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder,
+                    vim.tbl_extend("force", opts, { desc = "Remove workspace folder" }))
                 vim.keymap.set("n", "<leader>wl", function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, vim.tbl_extend("force", opts, { desc = "List workspace folders" }))
