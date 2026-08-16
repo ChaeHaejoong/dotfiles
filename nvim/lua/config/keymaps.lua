6e4bb6c5046ff92b4ddb19492b72a20281959092
ChaeHaejoong <svneh0329@naver.com>
2026-08-17 02:25:42 +0900

    add comma as a leader key

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 local keymap = vim.keymap.set
 
+-- Use comma as an alias for the Space leader key.
+keymap({ "n", "x" }, ",", "<Space>", { remap = true })
+
 keymap({ "n", "x" }, "<leader>w", "<C-w>", { desc = "Window commands", remap = true })
 keymap("n", "<leader>s", "<cmd>write<cr>", { desc = "Save file", silent = true, nowait = true })
 keymap("n", "<leader>S", "<cmd>wall<cr>", { desc = "Save all files", silent = true, nowait = true })
