a15dd169b52e842ec7d7dd7edc03cb7bce5480fa
ChaeHaejoong <svneh0329@naver.com>
2026-08-30 11:33:27 +0900

    oops

--- install.sh
+++ install.sh
@@ -13,22 +13,20 @@ configDirs=(
     waybar
 )
 
-if [ -e "$dst" ] || [ -L "$dst" ]; then
-    read -r -p "니 컴퓨터에 이미 설정 있는데 덮어쓸래 말래 [y/n]" answer
+for dir in "${configDirs[@]}"; do
+    src="$DOTFILES/$dir"
+    dst="$HOME/.config/$dir"
 
-    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-        for dir in "${configDirs[@]}"; do
-            src="$DOTFILES/$dir"
-            dst="$HOME/.config/$dir"
+    if [ -e "$dst" ] || [ -L "$dst" ]; then
+        read -r -p "$dst 이미 있는데 덮어쓸래? [y/n] " answer
 
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
             rm -rf "$dst"
             ln -s "$src" "$dst"
-        done
-
-        ln -s "$DOTFILES/.local/bin" "$HOME/.local/bin"
+        else
+            echo "$dst 건너뜀"
+        fi
     else
-        echo "설정 다운하지 않고 종료"
-        exit 0
+        ln -s "$src" "$dst"
     fi
-
-fi
+done
